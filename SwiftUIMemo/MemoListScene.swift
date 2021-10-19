d1dba5bf8210394348f4bf4d619e803d6c79ed11
hekang <glipung@naver.com>
2021-10-19 22:42:42 +0900

    first

--- SwiftUIMemo/MemoListScene.swift
+++ SwiftUIMemo/MemoListScene.swift
@@ -7,15 +7,16 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MemolistScene: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MemolistScene_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MemolistScene()
     }
 }
+
